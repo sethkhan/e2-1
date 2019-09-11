249aa6eba6519d9669bfc5dbbd1b537ca3e27d77
Susan Buck <susan@susanbuck.net>
2019-09-10 21:12:41 -0400

    Final touches on practice application

--- practice/index.php
+++ practice/index.php
@@ -7,6 +7,9 @@
 <body>
 	<h1>Practice Application</h1>
         <img src='/images/hes-logo.png'>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+<footer>&copy; 2019</footer>
 </body>
 </html>
 

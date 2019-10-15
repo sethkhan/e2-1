b83d2393016d936c5ec3817d74670fb4b0cd4b4f
Susan Buck <susan@susanbuck.net>
2019-10-15 12:20:05 -0400

    Week 7

---
+++ week7/index.php
@@ -0,0 +1,19 @@
+<?php require 'index-controller.php'; ?>
+<!doctype html>
+<html lang='en'>
+
+<head>
+
+    <title>Week 7</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+
+    <h1>Week 7 Practice</h1>
+
+
+</body>
+
+</html>
